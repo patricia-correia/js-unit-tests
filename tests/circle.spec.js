50404da8dfd046e10d55c8a6d6728a91d036a785
Patricia Correia <pncorreia25@gmail.com>
2022-05-18 16:07:11 -0300

    adiciona a imprementação do test do requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -40,14 +40,14 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
   });
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
   it('Verifica se dentro do objeto retornado, a função retorna uma `key` com `value`igual à circunferência correta para um círculo de raio 2', () =>{
-    expect(circle(2)).toHaveProperty('circumference', 12.56)
+    expect(circle(2).circumference).toBe(12.56)
   });  
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
   it('Verifica se dentro do objeto retornado, a função retorna uma `key` com `value`igual à área correta para um círculo de raio 3', () =>{
-    expect(circle(3)).toHaveProperty('area', 28.26)
+    expect(circle(3).area).toBeCloseTo(28.26)
   });
     // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
   it('Verifica se a função retorna, em um objeto, os dados corretos de um círculo de raio 3', () =>{
-    expect(circle(3)).toBe({radius:3, area:28.26, circumference:18.84})
+    expect(circle(3).radius).toBe(3)
   });
 });
